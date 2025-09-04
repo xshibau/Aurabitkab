31b96e1b60c2c4f23bfc6d253109324dfc92a4df
xshibau <ziugpro@gmail.com>
2025-09-04 13:52:41 +0700

    Create KeySystem_Aura.lua

---
+++ KeySystem_Aura.lua
@@ -0,0 +1,12 @@
+return {
+    script_key = "JRTtibghwXeykmqzh4r3oCvzf35xtb",
+    PremiumKeys = {
+        "JRTtibghwXeykmqzh4r3oCvzf35xtb",
+        "ABCD1234EFGH5678"
+    },
+    BlacklistKeys = {
+        ["abc123"] = "Hành vi gian lận bị phát hiện",
+        ["badkey456"] = "Vi phạm điều khoản sử dụng",
+        ["xyz789"] = "Key đã bị thu hồi do lạm dụng"
+    }
+}
