65a48b3e280dd4d554b0ca5e0e2369acf0fbd619
xshibau <ziugpro@gmail.com>
2025-09-04 14:31:43 +0700

    Update KeySystem_Aura.lua

--- KeySystem_Aura.lua
+++ KeySystem_Aura.lua
@@ -1,9 +1,9 @@
 return {
-    Keys = {
+    PremiumKeys = {
         "JRTtibghwXeykmqzh4r3oCvzf35xtb",
         "ABCD1234EFGH5678"
     },
-    Blacklist = {
+    BlacklistKeys = {
         ["abc123"] = "Hành vi gian lận bị phát hiện",
         ["badkey456"] = "Vi phạm điều khoản sử dụng"
     }
